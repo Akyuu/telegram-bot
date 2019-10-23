ea92478ecd842f9782401634e6886143da28b98b
sandtears <me@sandtears.com>
2019-10-23 19:02:40 +0800

    增加 user_info 处理方式

--- server.py
+++ server.py
@@ -14,12 +14,13 @@ import random
 import uuid
 import subprocess
 import requests
+import json
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
-def start(update, context):
+def start(update, _):
     readme = """大杂烩性质的Bot，目前功能包括：
 
 1. 将 Sticker 转换为 png 和 jpg 格式的文件
@@ -35,8 +36,6 @@ def start(update, context):
 
 
 def error(update, context):
-    user = update.message.from_user
-    username = user.username if user.username else user.first_name
     logging.error(str(update))
     start(update, context)
 
@@ -57,15 +56,16 @@ def password(update, context):
     username = user.username if user.username else user.first_name
     logging.info("[%s]: %s" % (username, update.message.text))
     try:
-        len = int(context.args[0])
-        if len > 1024: raise ValueError
+        length = int(context.args[0])
+        if length > 1024:
+            raise ValueError
         alphabet = string.ascii_letters + string.digits
-        update.message.reply_text("".join(random.choices(alphabet, k=len)))
+        update.message.reply_text("".join(random.choices(alphabet, k=length)))
     except ValueError:
         update.message.reply_text("/pw [length]")
 
 
-def sticker(update, context):
+def sticker(update, _):
     user = update.message.from_user
     username = user.username if user.username else user.first_name
     logging.info("[%s] sent a sticker." % username)
@@ -80,7 +80,7 @@ def sticker(update, context):
     subprocess.run('rm -f %s.webp %s.png' % (filename, filename), shell=True)
 
 
-def animation(update, context):
+def animation(update, _):
     user = update.message.from_user
     username = user.username if user.username else user.first_name
     logging.info("[%s] sent a gif." % username)
@@ -107,6 +107,15 @@ def animation(update, context):
     subprocess.run('rm -f %s.mp4 %s.gif' % (filename, filename), shell=True)
 
 
+def user_info(update, _):
+    user = update.message.from_user
+    username = user.username if user.username else user.first_name
+    logging.info("[%s] sent a gif." % username)
+    msg = json.dumps(str(user), sort_keys=True, indent=4, separators=(',', ': '))
+    msg = msg.encode('utf-8')
+    update.message.reply_text(msg)
+
+
 def main():
     # init
     updater = Updater(token=config.API_TOKEN, use_context=True)
@@ -119,6 +128,7 @@ def main():
     dp.add_handler(CommandHandler("pw", password))
     dp.add_handler(MessageHandler(Filters.sticker, sticker))
     dp.add_handler(MessageHandler(Filters.animation, animation))
+    dp.add_handler(MessageHandler(Filters.all, user_info))
 
     updater.start_polling()
     updater.idle()
@@ -126,4 +136,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
